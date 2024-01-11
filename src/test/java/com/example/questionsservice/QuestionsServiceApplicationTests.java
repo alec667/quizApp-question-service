64a639f2526f076f2c94a1db3843d03135af4c25
Alejandro Casiva <alec667@gmail.com>
2024-01-10 22:39:50 -0300

    test(QuizServiceApplicationTests): add context test

--- src/test/java/com/example/questionsservice/QuestionsServiceApplicationTests.java
+++ src/test/java/com/example/questionsservice/QuestionsServiceApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.questionsservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class QuestionsServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
 
 }
