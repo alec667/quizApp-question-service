ff44db0376bcbb5fd25be2af0f529128dcdb452f
Alejandro Casiva <alec667@gmail.com>
2024-01-24 01:24:55 -0300

    docs(QuestionsServiceApplication): add OpenAPI Bean for spring-docs

--- src/main/java/com/example/questionsservice/QuestionsServiceApplication.java
+++ src/main/java/com/example/questionsservice/QuestionsServiceApplication.java
@@ -1,13 +1,30 @@
 package com.example.questionsservice;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuestionsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionsServiceApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI questionsServiceOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("question service API")
+						.description("question service for quiz application")
+						.version("v1.0.0")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")))
+				.externalDocs(new ExternalDocumentation()
+						.description("SpringShop Wiki Documentation")
+						.url("https://springshop.wiki.github.org/docs"));
 	}
 
 }
